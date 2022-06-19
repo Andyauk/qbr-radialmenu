5a7517ade70b6c0fe9b3ed24cd74703e42a6fdcf
Andyauk <47583709+Andyauk@users.noreply.github.com>
2022-06-19 12:11:51 +0100

    code clean up

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,20 +9,14 @@ ui_page 'html/index.html'
 
 shared_scripts {
     'config.lua',
-    -- '@qb-core/shared/locale.lua',
-    -- 'locales/en.lua' -- Change this to your preferred language
 }
 
 client_scripts {
     'client/main.lua',
     'client/clothing.lua',
-    -- 'client/trunk.lua',
-    -- 'client/stretcher.lua'
 }
 
 server_scripts {
-    -- 'server/trunk.lua',
-    -- 'server/stretcher.lua'
 }
 
 files {
