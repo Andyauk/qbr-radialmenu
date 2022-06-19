9226e8c0cad397c29e6437fd17a477c4fdf8276e
Andyauk <47583709+Andyauk@users.noreply.github.com>
2022-06-19 12:27:42 +0100

    added F6 AS g conflits
    if you want to use G uncomment out the -- old G and comment out F6

--- client/main.lua
+++ client/main.lua
@@ -140,7 +140,8 @@ end
 CreateThread(function()
     while true do
         Citizen.Wait(7)
-if IsControlJustPressed(0, 0x760A9C6F) then
+--if IsControlJustPressed(0, 0x760A9C6F) then --Old G
+if IsControlJustPressed(0, 0x3C0A40F2) then -- F6
         setRadialState(true, true)
         SetCursorLocation(0.5, 0.5)
         end
